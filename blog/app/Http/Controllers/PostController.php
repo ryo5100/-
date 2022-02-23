7a767f3e8d2b0b16e0c3f1922021faa857ee475e
ryo5100 <ryo-.-5100@ezweb.ne.jp>
2022-02-23 11:54:40 +0900

    ブログ編集・保存

--- blog/app/Http/Controllers/PostController.php
+++ blog/app/Http/Controllers/PostController.php
@@ -27,4 +27,14 @@ class PostController extends Controller
    $post->fill($input)->save();
    return redirect('/posts/'.$post->id);
  }
+ public function edit(Post $post)
+ {
+   return view('edit')->with(['post' => $post]);
+ }
+ public function update(Request $request,Post $post)
+ {
+  $input = $request['post'];
+  $post->fill($input)->save();
+  return redirect('/posts/' . $post->id);
+ }
 }
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -14,6 +14,7 @@
 
 Route::get('/', "PostController@index");
 Route::get('/posts/create', "PostController@create");
+Route::get('/posts/{post}/edit', 'PostController@edit');
 Route::get('/posts/{post}', 'PostController@show');
-
 Route::post('/posts', "PostController@store");
+Route::put('/posts/{post}', 'PostController@update');
