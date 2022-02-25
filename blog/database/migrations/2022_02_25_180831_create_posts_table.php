63ab51053a0d3f72b07452284f99cd5c54d187bc
ryo5100 <ryo-.-5100@ezweb.ne.jp>
2022-02-25 19:04:49 +0900

    データベースバージョン管理

--- blog/database/migrations/2022_02_25_180831_create_posts_table.php
+++ blog/database/migrations/2022_02_25_180831_create_posts_table.php
@@ -14,8 +14,11 @@ class CreatePostsTable extends Migration
     public function up()
     {
         Schema::create('posts', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
+            $table->string('title', 50);
+            $table->string('body', 200);
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
